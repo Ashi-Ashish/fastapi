4094510e87e0e12ba8a90f6a13e8a4af21d79619
AshishRalh <46755420+AshishRalh@users.noreply.github.com>
2023-06-20 19:31:37 -0400

    NginX

--- app/main.py
+++ app/main.py
@@ -26,4 +26,4 @@ app.include_router(vote.router)
 # request Get method url: "/"
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World!!!!!!!!!!!!!!!!"}
